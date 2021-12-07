b884995462416a560bb28f0618da03d322c455b9
Marcus Billman <54668482+marcusbillman@users.noreply.github.com>
2021-12-07 12:41:17 +0100

    ✨ SendPage.vue: Create basic form with request

--- client/src/views/SendPage.vue
+++ client/src/views/SendPage.vue
@@ -1,7 +1,39 @@
-<script setup></script>
+<script setup>
+import { ref } from 'vue';
+
+const messageBodyRaw = ref('');
+
+async function handleSubmit(event) {
+  const message = {
+    body: messageBodyRaw.value,
+  };
+
+  await fetch('http://localhost:1234/api/v1/messages', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(message),
+  });
+
+  messageBodyRaw.value = '';
+}
+</script>
 
 <template>
-  <h1>Send</h1>
+  <div class="page">
+    <h1>Send</h1>
+    <form @submit.prevent="handleSubmit">
+      <textarea
+        v-model="messageBodyRaw"
+        name="body"
+        id="textarea"
+        cols="30"
+        rows="10"
+      ></textarea>
+      <button type="submit">Send</button>
+    </form>
+  </div>
 </template>
 
 <style></style>
