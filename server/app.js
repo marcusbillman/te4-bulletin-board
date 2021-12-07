31539cb2d6e6f7212d5da3ac25ee7f012c8d8145
Marcus Billman <54668482+marcusbillman@users.noreply.github.com>
2021-12-07 12:37:51 +0100

    ➕ Server: Add cors

--- server/app.js
+++ server/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
 const app = express();
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
