156df5d4285cb865ca1bab5606d8d7090f6c13ef
Marcus Billman <54668482+marcusbillman@users.noreply.github.com>
2021-12-08 09:58:01 +0100

    ✨ MessageCard: Display images

--- client/src/components/MessageCard.vue
+++ client/src/components/MessageCard.vue
@@ -6,17 +6,26 @@ defineProps(['body', 'type']);
 
 <template>
   <div class="card">
-    <p>{{ body }}</p>
+    <p v-if="type !== 'image'">{{ body }}</p>
     <vue-qrcode v-if="type === 'link'" :value="body"></vue-qrcode>
+    <img v-if="type === 'image'" :src="body" alt="Image" />
   </div>
 </template>
 
 <style>
 .card {
   height: 100%;
+  max-height: 30rem;
   padding: 1rem;
   border-radius: 0.5rem;
   background-color: #fafafa;
   margin-bottom: 1rem;
+}
+
+img {
+  width: 100%;
+  height: auto;
+  max-height: 100%;
+  object-fit: cover;
 }
 </style>
