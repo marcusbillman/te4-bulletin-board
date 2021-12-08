3317db1824bf71781d05f231b6145efd982965ad
Marcus Billman <54668482+marcusbillman@users.noreply.github.com>
2021-12-08 10:56:29 +0100

    ✨ BoardPage: Add header with QR code

--- client/src/views/BoardPage.vue
+++ client/src/views/BoardPage.vue
@@ -5,8 +5,9 @@ import io from 'socket.io-client';
 import MessageCard from '@/components/MessageCard.vue';
 
 const router = useRouter();
-console.log(router.currentRoute.value.query.server);
 const serverAddress = router.currentRoute.value.query.server;
+
+const sendPageUrl = location.href.replace('/board', '');
 
 // Populate messages with data from the database
 const messages = ref([]);
@@ -28,6 +29,26 @@ socket.on('message', (message) => {
 
 <template>
   <div class="page">
+    <header class="header">
+      <div class="header__left">
+        <h1>TE4 Bulletin Board</h1>
+      </div>
+      <div class="header__right">
+        <p class="header__instruction">Add stuff to this board</p>
+        <vue-qrcode
+          :value="sendPageUrl"
+          :options="{
+            scale: 3,
+            margin: 0,
+            color: {
+              dark: '#ffffff',
+              light: '#240b9a',
+            },
+          }"
+          class="header__qrcode"
+        ></vue-qrcode>
+      </div>
+    </header>
     <ul class="messages">
       <li v-for="message in messages" :key="message.id">
         <MessageCard :message="message" />
@@ -37,6 +58,29 @@ socket.on('message', (message) => {
 </template>
 
 <style>
+.header {
+  display: flex;
+  justify-content: space-between;
+  background: #240b9a;
+  color: white;
+  padding: 2rem;
+}
+
+.header__left {
+  display: flex;
+  align-items: center;
+}
+
+.header__right {
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+}
+
+.header__instruction {
+  font-size: 1.5rem;
+}
+
 .messages {
   font-size: 2rem;
   word-break: break-word;
