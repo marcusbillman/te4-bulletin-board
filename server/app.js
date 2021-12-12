5bd238f15b22a4d0e8e19863614a6135bde1efc0
Marcus Billman <54668482+marcusbillman@users.noreply.github.com>
2021-12-12 20:34:38 +0100

    ➖ Server: Remove unnecessary dependencies

--- server/app.js
+++ server/app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
-const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
 const app = express();
@@ -21,7 +20,6 @@ app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(function (req, res, next) {
