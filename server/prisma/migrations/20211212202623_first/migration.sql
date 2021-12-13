76e91f8523688902b1f4be26745a910509fee360
Marcus Billman <54668482+marcusbillman@users.noreply.github.com>
2021-12-13 20:56:33 +0100

    ✨ Server: Switch to PostgreSQL

--- server/prisma/migrations/20211212202623_first/migration.sql
+++ server/prisma/migrations/20211212202623_first/migration.sql
@@ -1,9 +1,11 @@
 -- CreateTable
 CREATE TABLE "Message" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "id" SERIAL NOT NULL,
     "body" TEXT NOT NULL,
     "type" TEXT NOT NULL,
     "pinned" BOOLEAN NOT NULL DEFAULT false,
     "author" TEXT NOT NULL,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Message_pkey" PRIMARY KEY ("id")
 );
